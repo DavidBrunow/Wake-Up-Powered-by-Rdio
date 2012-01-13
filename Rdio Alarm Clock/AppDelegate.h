82e9f1fefbac441880b071f6c232acc16654f4b7
David Brunow <davidbrunow@David-Brunows-MacBook.local>
2012-01-12 18:42:35 -0600

    Initial Commit

---
+++ Rdio Alarm Clock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rdio Alarm Clock
+//
+//  Created by David Brunow on 1/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rdio Alarm ClockTests/Rdio_Alarm_ClockTests.h
@@ -0,0 +1,13 @@
+//
+//  Rdio_Alarm_ClockTests.h
+//  Rdio Alarm ClockTests
+//
+//  Created by David Brunow on 1/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Rdio_Alarm_ClockTests : SenTestCase
+
+@end
