83622fc9f2a3dd058e0a1da7548e4f6f1e600d8f
David Brunow <david.brunow@me.com>
2012-03-18 22:16:24 -0500

    Initial commit

--- Rdio Alarm Clock/AppDelegate.h
+++ Rdio Alarm Clock/AppDelegate.h
@@ -7,9 +7,27 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Rdio/Rdio.h>
+#import "Reachability.h"
+#import "MainViewController.h"
+#import "MMPDeepSleepPreventer.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : NSObject <UIApplicationDelegate>
+{
+    Rdio *rdio;
+    Reachability *internetReachable;
+    Reachability *hostReachable;
+    MainViewController *mainView;
+    UIWindow *window;
+}
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) MainViewController *mainView;
+@property (strong, nonatomic) MMPDeepSleepPreventer *awake;
+@property (readonly, retain) Rdio *rdio;
+
++(Rdio *)rdioInstance;
+
+- (void) checkNetworkStatus:(NSNotification *)notice;
 
 @end
