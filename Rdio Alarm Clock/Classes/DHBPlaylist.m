6c5bcfd7e2eb2a9544f4b6c3439bd25ff6ce9b91
David Brunow <david.brunow@me.com>
2013-04-07 19:05:01 -0500

    Refactor code to remove tracks that cannot be streamed

--- Rdio Alarm Clock/Classes/DHBPlaylist.m
+++ Rdio Alarm Clock/Classes/DHBPlaylist.m
@@ -23,23 +23,32 @@
     
     if ([method isEqualToString:@"get"]) {
         for(NSString *key in [data allKeys]) {
+            //NSLog(@"CanStream: %@", [[data objectForKey:key] objectForKey:@"canStream"]);
             if ([[[data objectForKey:key] objectForKey:@"canStream"] isEqual:[NSNumber numberWithBool:YES]]) {
             } else {
-                [self.trackKeys removeObjectIdenticalTo:key];
+                //NSLog(@"Number of trackKeys: %d - current key: %@", [self.trackKeys count], key);
+                for(int x = 0; x < self.trackKeys.count; x++) {
+                    if([[self.trackKeys objectAtIndex:x] isEqualToString:key]) {
+                        [self.trackKeys removeObjectAtIndex:x];
+                    }
+                }
+                //NSLog(@"Number of trackKeys: %d - current key: %@", [self.trackKeys count], key);
             }
         }
     }
+    //NSLog(@"Got tracks for playlist name: %@", self.playlistName);
     /*
+    int x = 0;
     for(NSString *trackKey in self.trackKeys) {
-        NSLog(@"Key: %@", trackKey);
+        NSLog(@"Key %d: %@", x, trackKey);
+        x++;
     }
     */
 }
 
 - (void)rdioRequest:(RDAPIRequest *)request didFailWithError:(NSError*)error {
-    if([[[error userInfo] objectForKey:@"NSLocalizedDescription"] isEqualToString:@"The request timed out."]) {
-        [self determineStreamableSongs];
-    }
+    //NSLog(@"Got error on playlist name: %@", self.playlistName);
+    [[AppDelegate rdioInstance] callAPIMethod:@"get" withParameters:request.parameters delegate:self];
 }
 
 - (void)setTrackKeys:(NSMutableArray *)trackKeys clean:(bool) isClean
@@ -55,13 +64,48 @@
 {
     self.trackKeys = [self removeDuplicatesInPlaylist:self.trackKeys];
 
-    NSString *songsToPlayString = [self.trackKeys objectAtIndex:0];
-    for (int x = 1; x < self.trackKeys.count; x++) {
-        songsToPlayString = [NSString stringWithFormat:@"%@, %@", songsToPlayString, [self.trackKeys objectAtIndex:x]];
-    }
+    NSString *songsToPlayString = @"";
+    int totalNumberOfTracks = self.trackKeys.count;
+    int chunkSize = 250;
     
-    NSDictionary *trackInfo = [[NSDictionary alloc] initWithObjectsAndKeys:songsToPlayString, @"keys", @"canStream", @"type", nil];
-    [[AppDelegate rdioInstance] callAPIMethod:@"get" withParameters:trackInfo delegate:self];
+    if(totalNumberOfTracks < chunkSize) {
+        for (int x = 0; x < self.trackKeys.count; x++) {
+            if([songsToPlayString isEqualToString:@""]) {
+                songsToPlayString = [NSString stringWithFormat:@"%@", [self.trackKeys objectAtIndex:x]];
+            } else {
+                songsToPlayString = [NSString stringWithFormat:@"%@, %@", songsToPlayString, [self.trackKeys objectAtIndex:x]];
+            }
+        }
+        NSDictionary *trackInfo = [[NSDictionary alloc] initWithObjectsAndKeys:songsToPlayString, @"keys", @"0", @"canStream", nil];
+        [[AppDelegate rdioInstance] callAPIMethod:@"get" withParameters:trackInfo delegate:self];
+    } else {
+        int numberOfChunks = totalNumberOfTracks / chunkSize;
+        int numberInLastChunk = totalNumberOfTracks - (numberOfChunks * chunkSize);
+        
+        for (int x = 0; x <= numberOfChunks; x++) {
+            for (int y = x * chunkSize; y < numberOfChunks * chunkSize; y++) {
+                if([songsToPlayString isEqualToString:@""]) {
+                    songsToPlayString = [NSString stringWithFormat:@"%@", [self.trackKeys objectAtIndex:y]];
+                } else {
+                    songsToPlayString = [NSString stringWithFormat:@"%@, %@", songsToPlayString, [self.trackKeys objectAtIndex:y]];
+                //NSLog(@"Submitted Key %d: %@", y, [self.trackKeys objectAtIndex:y]);
+                }
+            }
+            NSDictionary *trackInfo = [[NSDictionary alloc] initWithObjectsAndKeys:songsToPlayString, @"keys", @"0", @"canStream", nil];
+            [[AppDelegate rdioInstance] callAPIMethod:@"get" withParameters:trackInfo delegate:self];
+            songsToPlayString = @"";
+        }
+        for(int y = numberOfChunks * chunkSize; y < numberOfChunks * chunkSize + numberInLastChunk; y++) {
+            if([songsToPlayString isEqualToString:@""]) {
+                songsToPlayString = [NSString stringWithFormat:@"%@", [self.trackKeys objectAtIndex:y]];
+            } else {
+                songsToPlayString = [NSString stringWithFormat:@"%@, %@", songsToPlayString, [self.trackKeys objectAtIndex:y]];
+                //NSLog(@"Submitted Key %d: %@", y, [self.trackKeys objectAtIndex:y]);
+            }
+        }
+        NSDictionary *trackInfo = [[NSDictionary alloc] initWithObjectsAndKeys:songsToPlayString, @"keys", @"0", @"canStream", nil];
+        [[AppDelegate rdioInstance] callAPIMethod:@"get" withParameters:trackInfo delegate:self];
+    }
 }
 
 - (NSMutableArray *) removeDuplicatesInPlaylist: (NSMutableArray *) playlist
